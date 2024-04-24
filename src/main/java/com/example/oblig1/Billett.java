eb048e123eb2b52468ad3883ef440b2bb552132e
Olivereliasandersen <oland0229@oslomet.no>
2024-04-24 17:32:27 +0200

    asda

--- src/main/java/com/example/oblig1/Billett.java
+++ src/main/java/com/example/oblig1/Billett.java
@@ -1,4 +1,72 @@
 package com.example.oblig1;
 
 public class Billett {
+    private String film;
+    private int antall;
+    private String fornavn;
+    private String etternavn;
+    private String telefonNr;
+    private String epost;
+
+
+    public Billett(String film, int antall, String fornavn, String etternavn, String telefonNr, String epost){
+        this.film = film;
+        this.antall = antall;
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.telefonNr = telefonNr;
+        this.epost = epost;
+    }
+
+    public String getFilm() {
+        return film;
+    }
+
+    public void setFilm(String film) {
+        this.film = film;
+    }
+
+    public int getAntall() {
+        return antall;
+    }
+
+    public void setAntall(int antall) {
+        this.antall = antall;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+
+    public void setEtternavn(String etternavn) {
+        this.etternavn = etternavn;
+    }
+
+    public String getTelefonNr() {
+        return telefonNr;
+    }
+
+    public void setTelefonNr(String telefonNr) {
+        this.telefonNr = telefonNr;
+    }
+
+    public String getEpost() {
+        return epost;
+    }
+
+    public void setEpost(String epost) {
+        this.epost = epost;
+    }
+
+    public String toString(){
+        return film +", Antall : "+antall+ ", Navn : " + fornavn + " " + etternavn + ", TelefonNr : " + telefonNr + ", Epost : " + epost;
+    }
 }
